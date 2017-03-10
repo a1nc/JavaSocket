46d161e98cf67d5433b997fec9852319c18a43bc
YangPeng <ironmaxmax@outlook.com>
2017-03-11 00:43:31 +0800

    Create SocketServer.java
    http://www.xuebuyuan.com/1998051.html

---
+++ SocketServer.java
@@ -0,0 +1,45 @@
+//http://www.xuebuyuan.com/1998051.html
+
+import java.io.*;
+import java.net.*;
+
+public class SocketServer{
+    public static void main(String [] args)
+    {
+        try{
+            ServerSocket server=null;
+            try{
+                server=new ServerSocket(4700);
+            }catch (Exception e)
+            {
+                System.out.println("can not listen to:"+e);
+            }
+            Socket socket=null;
+            try{
+                socket=server.accept();
+            }catch (Exception e)
+            {
+                System.out.println("Error."+e);
+            }
+            String line;
+            BufferedReader is=new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            PrintWriter os=new PrintWriter(socket.getOutputStream());
+            BufferedReader sin=new BufferedReader(new InputStreamReader(System.in));
+            System.out.println("Client:"+is.readLine());
+            line=sin.readLine();
+            while(!line.equals("!quit")){
+                os.println(line);
+                os.flush();
+                System.out.println("Server:"+line);
+                System.out.println("Client:"+is.readLine());
+                line=sin.readLine();
+            }
+            os.close();
+            is.close();
+            socket.close();
+            server.close();
+        }catch (Exception e){
+            System.out.println("Error:"+e);
+        }
+    }
+}
