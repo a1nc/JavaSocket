bc7da6e5ac13e3dcdcb36516f333b4a5713fe817
YangPeng <ironmaxmax@outlook.com>
2017-04-07 14:21:49 +0800

    Add files via upload

---
+++ Day6/loginwindow.cpp
@@ -0,0 +1,56 @@
+#include "loginwindow.h"
+#include "ui_loginwindow.h"
+#include <QDebug>
+
+LoginWindow::LoginWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::LoginWindow)
+{
+    ui->setupUi(this);
+    this->setFixedSize(this->width(),this->height());
+    //qDebug()<<this->width()<<" "<<this->height();
+    LoginButton = new QPushButton(this);
+    LoginButton->setText("Lo...");
+
+    UserNameEdit = new QLineEdit(this);
+    UserPswdEdit = new QLineEdit(this);
+    UserPswdEdit->setEchoMode(QLineEdit::Password);
+
+    NameLabel = new QLabel(this);
+    NameLabel->setText("Name");
+    PswdLabel = new QLabel(this);
+    PswdLabel->setText("Pswd");
+
+    NameLabel->setGeometry(QRect(70,100,50,25));
+    PswdLabel->setGeometry(QRect(70,130,50,25));
+    UserNameEdit->setGeometry(QRect(110,100,200,25));
+    UserPswdEdit->setGeometry(QRect(110,130,200,25));
+    LoginButton->setGeometry(QRect(180,200,50,25));
+}
+
+LoginWindow::~LoginWindow()
+{
+    delete ui;
+}
+
+void LoginWindow::GetName(void)
+{
+    QString Name;
+    Name = UserNameEdit->text();
+    qDebug()<<Name;
+    LoginWindow::PostName(Name);
+}
+
+void LoginWindow::GetPswd(void)
+{
+    QString Pswd;
+    Pswd = UserPswdEdit->text();
+    qDebug()<<Pswd;
+    LoginWindow::PostPswd(Pswd);
+}
+
+void LoginWindow::UserWindowShow()
+{
+    LoginWindow::close();
+    uw.show();
+}
